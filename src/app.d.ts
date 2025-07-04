27fc866aee840e73095cd8b488b542b5219153f3
Gustavo Fragoso <gustavo-gbf@overmind.ai>
2025-07-04 19:50:16 -0300

    first commit

---
+++ src/app.d.ts
@@ -0,0 +1,15 @@
+// See https://svelte.dev/docs/kit/types#app.d.ts
+// for information about these interfaces
+declare global {
+	namespace App {
+		interface Locals {
+			user: import('$lib/authorization/auth').SessionValidationResult['user'];
+			session: import('$lib/authorization/auth').SessionValidationResult['session'];
+		}
+	} // interface Error {}
+	// interface Locals {}
+} // interface PageData {}
+// interface PageState {}
+
+// interface Platform {}
+export {};
