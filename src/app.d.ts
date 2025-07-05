ff8b08369af0f118a5981db5a14475f30b101e28
Gustavo Fragoso <gustavo-gbf@overmind.ai>
2025-07-05 09:58:57 -0300

    Reverting first commit

--- src/app.d.ts
+++ src/app.d.ts
@@ -2,14 +2,12 @@
 // for information about these interfaces
 declare global {
 	namespace App {
-		interface Locals {
-			user: import('$lib/authorization/auth').SessionValidationResult['user'];
-			session: import('$lib/authorization/auth').SessionValidationResult['session'];
-		}
-	} // interface Error {}
-	// interface Locals {}
-} // interface PageData {}
-// interface PageState {}
+		// interface Error {}
+		// interface Locals {}
+		// interface PageData {}
+		// interface PageState {}
+		// interface Platform {}
+	}
+}
 
-// interface Platform {}
 export {};
