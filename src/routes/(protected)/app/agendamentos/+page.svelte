6b45bab6116b07f1749182f0b458c55f36f7ed9b
Gustavo Fragoso <gustavo-gbf@overmind.ai>
2025-07-08 12:30:01 -0300

    schedulling

---
+++ src/routes/(protected)/app/agendamentos/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { Calendar, TimeGrid } from '@event-calendar/core';
+
+	let options = $state({
+		view: 'timeGridWeek',
+		events: [
+			// your list of events
+		]
+	});
+</script>
+
+<div class="card p-4">
+	<Calendar plugins={[TimeGrid]} {options} />
+</div>
