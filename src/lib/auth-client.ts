1db7c8b8a2a353888fd673fe802f28c60582af9b
Gustavo Fragoso <gustavo-gbf@overmind.ai>
2025-07-15 21:01:51 -0300

    updating database

--- src/lib/auth-client.ts
+++
@@ -1,6 +0,0 @@
-import { createAuthClient } from 'better-auth/svelte';
-import { apiKeyClient } from 'better-auth/client/plugins';
-
-export const authClient = createAuthClient({
-	plugins: [apiKeyClient()]
-});
